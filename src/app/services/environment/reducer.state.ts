51649645d96d6ea681e8bf5172f994f4b09d9120
Heejae Jang <j132134@gmail.com>
2019-01-08 11:15:26 +0900

    Remove src/types/ directory

--- src/app/services/environment/reducer.state.ts
+++ src/app/services/environment/reducer.state.ts
@@ -1,6 +1,17 @@
-import { EnvironmentDTO } from '../../../types';
 
-export type EnvironmentState = EnvironmentDTO;;
+export type EnvironmentState = {
+  platform: {
+    isRidiApp: boolean;
+  };
+  constants: {
+    BASE_URL_STORE: string;
+    BASE_URL_RIDISELECT: string;
+    BASE_URL_RIDI_PAY_API: string;
+    BASE_URL_STATIC: string;
+    FREE_PROMOTION_MONTHS: number;
+    OAUTH2_CLIENT_ID: string;
+  };
+};;
 
 export const environmentInitialState: EnvironmentState = {
   platform: {
