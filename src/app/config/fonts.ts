128a2b3665010dc5ea24b68186714c664b407385
Heejae Jang <j132134@gmail.com>
2019-01-16 14:23:38 +0900

    Fix: webpack load from index.ts

--- src/app/config/fonts.ts
+++ src/app/config/fonts.ts
@@ -1,9 +1,11 @@
 import * as WebFont from 'webfontloader';
 
-WebFont.load({
-  custom: {
-    families: ['Noto Sans KR:n4,n7,n9', 'Minion Pro', 'review_num'],
-    urls: ["/assets/font.css"],
-  },
-  timeout: 60000,
-});
+export const loadFonts = () => {
+  WebFont.load({
+    custom: {
+      families: ['Noto Sans KR:n4,n7,n9', 'Minion Pro', 'review_num'],
+      urls: ["/assets/font.css"],
+    },
+    timeout: 60000,
+  });
+};
