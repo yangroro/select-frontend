0d9792ae2acbb84aaec0fef06e317ddbe31d0260
Heejae Jang <j132134@gmail.com>
2019-01-16 16:02:24 +0900

    Fix: tslint config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   linters: {
-    'src/app/**/*.{ts,tsx}': ['tslint -c ./tslint.js', 'git add'],
+    'src/app/**/*.{ts,tsx}': ['tslint -c ./tslint.js --fix', 'git add'],
     'src/css/**/*.css': ['stylelint --fix', 'git add'],
   },
 };
