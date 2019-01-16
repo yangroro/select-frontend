8bd0c02613d3160a4b116c38a8029001c2323bfe
Heejae Jang <j132134@gmail.com>
2019-01-16 13:47:35 +0900

    Fix: webfont

---
+++ src/app/config/fonts.ts
@@ -0,0 +1,9 @@
+import * as WebFont from 'webfontloader';
+
+WebFont.load({
+  custom: {
+    families: ['Noto Sans KR:n4,n7,n9', 'Minion Pro', 'review_num'],
+    urls: ["/assets/font.css"],
+  },
+  timeout: 60000,
+});
