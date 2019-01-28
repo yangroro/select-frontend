0f885b57ce540a1caf9a4dac0b7fb4df476225f0
이준우 <junwoo.lee@ridi.com>
2019-01-28 19:12:48 +0900

    집앞 서점 신간 영역 예외처리 실서버와 동일하게 수정.

--- src/app/services/selection/sagas.ts
+++ src/app/services/selection/sagas.ts
@@ -16,7 +16,11 @@ export function* loadSelection({ payload }: ReturnType<typeof Actions.loadSelect
     }
   } catch (e) {
     yield put(Actions.loadSelectionFailure({ selectionId, page }));
-    callbackAfterFailedFetch(e, page);
+    if (selectionId !== 'hotRelease') {
+      // hotRelease의 경우 홈 화면에서만 섹션이 노출되고 아직 전체보기 페이지가 없어서 페이지네이션의 개념이 없음....
+      // 오픈 전에는 데이터가 없기 때문에 뒤로가기 처리가 되면 안되는 이유로 예외처리.
+      callbackAfterFailedFetch(e, page);
+    }
   }
 }
 
