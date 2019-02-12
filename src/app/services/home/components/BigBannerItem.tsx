218fe118853719aaddc45b76cc37791825f385cf
이준우 <junwoo.lee@ridi.com>
2019-02-12 15:42:08 +0900

    빅배너 + 도서 상세페이지 내용에서 (외부)링크 제거 처리.

--- src/app/services/home/components/BigBannerItem.tsx
+++ src/app/services/home/components/BigBannerItem.tsx
@@ -1,4 +1,5 @@
 import { EnvironmentState } from 'app/services/environment';
+import { getIsIosInApp } from 'app/services/environment/selectors';
 import { RidiSelectState } from 'app/store';
 import * as React from 'react';
 import { connect } from 'react-redux';
@@ -6,6 +7,7 @@ import { Link } from 'react-router-dom';
 
 interface BigBannerItemStateProps {
   env: EnvironmentState;
+  isIosInApp: boolean;
 }
 
 interface BigBannerItemProps {
@@ -16,25 +18,31 @@ interface BigBannerItemProps {
 type Props = BigBannerItemStateProps & BigBannerItemProps;
 
 export const BigBannerItem: React.SFC<Props> = (props) => {
-  const { onClick, linkUrl, children } = props;
+  const { isIosInApp, onClick, linkUrl, children, env } = props;
   const compProps = {
     className: 'BigBanner_Item',
     onClick,
     children,
   };
-  const ridiselectReg = new RegExp(`^https?:${props.env.SELECT_URL}/`);
+  const ridiselectReg = new RegExp(`^https?:${env.SELECT_URL}/`);
   const schemeReg = /^https?:\/\//;
 
   if (ridiselectReg.test(linkUrl) || !schemeReg.test(linkUrl)) {
     const linkProps = { ...compProps, to: linkUrl };
     return <Link {...linkProps}/>;
   }
+  if (isIosInApp) {
+    return null;
+  }
   const anchorProps = { ...compProps, href: linkUrl };
   return <a {...anchorProps}/>;
 };
 
 const mapStateToProps = (state: RidiSelectState, ownProps: BigBannerItemProps): BigBannerItemStateProps => {
-  return { env: state.environment };
+  return {
+    env: state.environment,
+    isIosInApp: getIsIosInApp(state),
+  };
 };
 
 export const ConnectedBigBannerItem = connect(mapStateToProps, null)(BigBannerItem);
