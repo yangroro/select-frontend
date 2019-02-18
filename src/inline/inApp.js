cd4514b917d4061fd0f10c269029cfe2b4bd2a79
이준우 <junwoo.lee@ridi.com>
2019-02-18 18:56:10 +0900

    raw-loader 사용하도록 수정.

--- src/inline/inApp.js
+++ src/inline/inApp.js
@@ -1,14 +1,14 @@
-const isIos = /iphone|ipad|ipod/i.test(window.navigator.userAgent);
-const isRidibooks = /ridibooks\/[0-9]+\.?[0-9]*/i.test(window.navigator.userAgent);
 function isLoginRequired(url) {
   return false;
 };
 function isExternalLink(url) {
-  return !new RegExp("<% print(host.replace('https://', '^((https?:)?\/\/)?').replace(/\./g, '\\\\.')) %>").test(url);
+  return !new RegExp(window.__RIDISELECT_HOST__.replace('https://', '^((https?:)?\/\/)?').replace(/\./g, '\\\\.')).test(url);
 };
 
-if (isIos && isRidibooks) {
-  document.body.classList.add('iosApp');
-} else if (isRidibooks) {
-  document.body.classList.add('androidApp');
+if (/ridibooks\/[0-9]+\.?[0-9]*/i.test(window.navigator.userAgent)) {
+  if (/iphone|ipad|ipod/i.test(window.navigator.userAgent)) {
+    document.body.classList.add('iosApp');
+  } else {
+    document.body.classList.add('androidApp');
+  }
 }
