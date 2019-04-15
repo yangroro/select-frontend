e85e72dd79f89d561e80a4a2c09083a0562c93cf
오민형 <minhyung@ridi.com>
2019-04-15 15:11:56 +0900

    만료일 계산 로직 추가

---
+++ src/app/utils/expiredDate.ts
@@ -0,0 +1,20 @@
+import { differenceInDays, differenceInMinutes, parse } from 'date-fns';
+
+export function getExpiredDate(BookExpiredDate: string, NextBillDate: string) {
+  const currentDate = new Date();
+  const bookEndDate = parse(BookExpiredDate);
+  /* 만료일이 25일 이상일 때는 표시 안해줘도 됨 */
+  if (differenceInDays(bookEndDate, currentDate) > 25) {
+    return '';
+  }
+  const endDate: Date = parse(NextBillDate);
+  const differenceMinutes: number = differenceInMinutes(endDate, currentDate);
+  /* 1일: 1440분, 1시간: 60분 */
+  const expiredDays = Math.floor(differenceMinutes / 1440);
+  const expiredHours = Math.floor((differenceMinutes % 1440) / 60);
+  const expiredMinutes = Math.floor((differenceMinutes % 1440) % 60);
+
+  const expiredDate = `${expiredDays}일 ${expiredHours}시간 ${expiredMinutes}분 남음`;
+
+  return expiredDate;
+}
