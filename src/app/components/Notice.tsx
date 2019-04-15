d43dff8ff1c83a6013107462a63b681881e97f32
오민형 <minhyung@ridi.com>
2019-04-15 15:47:48 +0900

    도서상세페이지 Notice Component 작성

---
+++ src/app/components/Notice.tsx
@@ -0,0 +1,31 @@
+import { Icon } from '@ridi/rsg';
+import * as React from 'react';
+
+const { Fragment } = React;
+
+export interface NoticeProps {
+  type?: string;
+  icon?: string;
+  mainText: string;
+  subText?: string;
+  subTextType?: string;
+}
+
+export const Notice: React.SFC<NoticeProps> = (props) => {
+  const { mainText, subText } = props;
+
+  return (
+    <div className={'Notice'}>
+      <Icon name="info_2" />
+      <div className={'Notice_Main_Text'}>
+      {mainText}
+      { subText &&
+        <span className={'Sub_Notice'}>
+          <Icon name="won" />
+          <span className={'Notice_Sub_Text'}>{subText}</span>
+        </span>
+      }
+      </div>
+    </div>
+  );
+};
