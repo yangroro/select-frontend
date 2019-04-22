17c5fbf540d672e6aa5cfd1ead2e9f6e0dc82feb
이준우 <junwoo.lee@ridi.com>
2019-04-22 16:25:42 +0900

    HelmetWithTitle 컴포넌트 Connected로 받고있는 것 수정.

--- src/app/scenes/ExpiredBook.tsx
+++ src/app/scenes/ExpiredBook.tsx
@@ -1,5 +1,5 @@
 import { Button, Empty, Icon } from '@ridi/rsg';
-import { ConnectedHelmetWithTitle } from 'app/components';
+import { HelmetWithTitle } from 'app/components';
 import { PageTitleText } from 'app/constants';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
@@ -9,7 +9,7 @@ export class ExpiredBook extends React.Component {
   public render() {
     return (
       <main className="SceneWrapper">
-        <ConnectedHelmetWithTitle titleName={PageTitleText.EXPIRED} />
+        <HelmetWithTitle titleName={PageTitleText.EXPIRED} />
         <section className="PageExpireBook">
           <Empty className={'Empty_HasButton'} description="이 책은 출판사 또는 저작권자의 요청으로 서비스가 중지되어 책 정보를 볼 수 없습니다." iconName="book_1" />
           <Link to={`/home`} className="MySelectBookList_Link">
