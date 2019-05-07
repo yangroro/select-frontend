9a1099f9602f42e3835ee043dbc9c6eb0bcba3b4
이준우 <junwoo.lee@ridi.com>
2019-05-07 17:07:44 +0900

    도서가 닫힌경우 뜨는 화면 줄바꿈 처리 추가.

--- src/app/scenes/NotAvailableBook.tsx
+++ src/app/scenes/NotAvailableBook.tsx
@@ -1,18 +1,27 @@
-import { Button, Empty, Icon } from '@ridi/rsg';
-import { HelmetWithTitle } from 'app/components';
-import { PageTitleText } from 'app/constants';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-export class NotAvailableBook extends React.Component {
+import { Button, Empty, Icon } from '@ridi/rsg';
+import { HelmetWithTitle } from 'app/components';
+import { PageTitleText, RoutePaths } from 'app/constants';
 
+export class NotAvailableBook extends React.Component {
   public render() {
     return (
       <main className="SceneWrapper">
         <HelmetWithTitle titleName={PageTitleText.NOT_AVAILABLE_BOOK} />
         <section className="PageExpireBook">
-          <Empty className={'Empty_HasButton'} description="이 책은 출판사 또는 저작권자의 요청으로 서비스가 중지되어 책 정보를 볼 수 없습니다." iconName="book_1" />
-          <Link to={`/home`} className="MySelectBookList_Link">
+          <Empty
+            className="Empty_HasButton"
+            description={(
+              <>
+                이 책은 출판사 또는 저작권자의 요청으로 <br />
+                서비스가 중지되어 책 정보를 볼 수 없습니다.
+              </>
+            )}
+            iconName="book_1"
+          />
+          <Link to={RoutePaths.HOME} className="MySelectBookList_Link">
             <Button
               color="blue"
               outline={true}
